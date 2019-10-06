8e2ddae08f76217643067af42e92887e1dbad794
Stenia Felix <steniafelix@gmail.com>
2019-10-06 15:04:01 -0300

    Importing routes and reactronConfig

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Router } from "react-router-dom";
 
-import Routes from "./routes/index";
+import "./config/ReactotronConfig";
+
+import Routes from "./routes";
 import history from "./services/history";
 
 import GlobalStyle from './styles/global';
