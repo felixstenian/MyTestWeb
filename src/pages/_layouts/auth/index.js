e9e62e691a1009a435eea88df453ea25068213d8
Stenia Felix <steniafelix@gmail.com>
2019-10-06 23:52:57 -0300

    adding layouts

---
+++ src/pages/_layouts/auth/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Wrapper } from './styles';
+
+export default function AuthLayout({ children }) {
+  return (
+    <Wrapper>{children}</Wrapper>
+  );
+}
+
+AuthLayout.proptTypes = {
+  children: PropTypes.element.isRequired,
+}
