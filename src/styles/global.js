da2b0ed3b450b1c106aa1606d91697e7ee98e876
Stenia Felix <steniafelix@gmail.com>
2019-10-08 13:41:02 -0300

    Configuring global styles

---
+++ src/styles/global.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+import "react-toastify/dist/ReactToastify.css";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: none;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
