cd0a5699b81b4b16debe2cfa8d2f226a9c806dc7
Stenia Felix <steniafelix@gmail.com>
2019-10-08 13:38:21 -0300

    Configuring page profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
 
-// import { Container } from './styles';
+
+import { signOut } from "../../store/modules/auth/actions";
+
+import { Container } from './styles';
+
 
 export default function Profile() {
+  const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
+ 
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
-    <div />
-  );
+    <Container>
+    <div>
+      <img src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="mytest"/>
+      <ul>
+        
+        <li><small>Nome: </small>{profile.name}</li>
+        <li><small>Usuário: </small>{profile.user_name}</li>
+        <li><small>Email: </small>{profile.email}</li>
+      </ul>
+    </div>
+    
+    <button type="submit" onClick={handleSignOut}>Sair</button>
+  </Container>
+  )
 }
