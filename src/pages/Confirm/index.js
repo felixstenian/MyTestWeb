0bb13aa72790100b65903ef4f0465ca368b9e04e
Stenia Felix <steniafelix@gmail.com>
2019-10-08 16:30:59 -0300

    Adjustings

--- src/pages/Confirm/index.js
+++ src/pages/Confirm/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 // import { Container } from './styles';
 
 export default function Confirm() {
   return (
-    <h1>Confirmar Cadastro</h1>,
+    <>
+      <h1>Confirmar Cadastro</h1>
 
-    <p>Acesse a caixa de entrada do seu email e abra o email de confirmação com o assunto "Confirmar Cadastro"</p>
+      <hr/>
+
+      <p>Acesse a caixa de entrada do seu email e abra o email de confirmação com o assunto "Confirmar Cadastro"</p>
+
+      <Link to="/entrar"><button type="submit">Login</button></Link>
+    </>
   );
 }
