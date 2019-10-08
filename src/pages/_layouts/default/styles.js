824c9739d763565036df252f624ac13b4ace3cb9
Stenia Felix <steniafelix@gmail.com>
2019-10-08 13:39:49 -0300

    Configuring styles default

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
-  background: #333;
+  height: 125%;
+  background: linear-gradient(-90deg, rgb(50, 127, 290), #00BFFF);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
