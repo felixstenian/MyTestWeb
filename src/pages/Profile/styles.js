3921b58790eb08fc034fcb0f91b0d3436dd30eb2
Stenia Felix <steniafelix@gmail.com>
2019-10-08 13:43:17 -0300

    Adding profile styles

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { darken } from "polished";
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  div {
+    width: 90%;
+    border: 0;
+    border-radius: 20px;
+    padding: 20px;
+    background: #fff;
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    img {
+      margin-right:20px;
+      height: 120px;
+      width: 120px;
+      border-radius: 50%;
+      border: 1px solid rgba(255,255,255, 0.3);
+      background: #eee;
+    }
+
+    ul {
+      font-size: 20px;
+      color: #333;
+      
+
+      li {
+        margin-bottom: 10px;
+      }
+
+      small {
+        font-size: 14px;
+        color: #999;
+        justify-content: left;
+        text-align: center;
+      }
+    }
+  }
+
+
+  button {
+    width: 80%;
+    margin: 20px 0 0;
+    height: 44px;
+    background: #f64c75;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+    align-self: center;
+    align-items: center;
+
+    &:hover {
+      background: ${darken(0.09, "#f64c75")}
+    }
+}
+  
+`;
