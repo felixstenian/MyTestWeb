66fbb0d8fbd73ef28535c3f964d319e0c8830a26
Stenia Felix <steniafelix@gmail.com>
2019-10-08 13:40:04 -0300

    Configuring file route

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
+import React  from 'react'
 import PropTypes from "prop-types";
 import { Route, Redirect } from "react-router-dom";
 
+import AuthLayout from "../pages/_layouts/auth";
+import DefaultLayour from "../pages/_layouts/default";
+
+import { store } from "../store";
+
 export default function RouteWrapper({
-  component = Component,
-  isPrivate = false,
+  component: Component,
+  isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />
@@ -17,10 +22,20 @@ export default function RouteWrapper({
     return <Redirect to="perfil" />
   }
 
-  return (
-    <Route 
-      {...rest}
-      component={Component}
-    />
-  )
+  const Layout = signed ? DefaultLayour : AuthLayout;
+
+  return <Route {...rest} render={props => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  )} />;
 }
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
