8b5031368c2ee8fb840f9aaa0cd14e2bebc2686f
Stenia Felix <steniafelix@gmail.com>
2019-10-08 13:40:47 -0300

    Configuring file routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,31 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from "./Route";
 
 import Main from "../pages/Main";
 import Profile from "../pages/Profile";
+import EditProfile from "../pages/EditProfile";
 
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 
 import Active from "../pages/Active";
+import Confirm from "../pages/Confirm";
+
 
 
 export default function Routes() {
   return (
-    <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main}/>
         <Route path="/perfil" component={Profile} isPrivate/>
+        <Route path="/editarperfil" component={EditProfile} isPrivate/>
 
         <Route path="/criarconta" component={SignUp}/>
         <Route path="/entrar" component={SignIn}/>
 
         <Route path="/ativacao" component={Active}/>
+        <Route path="/confirmar" component={Confirm}/>
       </Switch>
-    </BrowserRouter>
   )
 }
