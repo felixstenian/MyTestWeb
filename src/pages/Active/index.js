1f540f8fb56223511ef57f3e3df4b9a0ce83b46a
Stenia Felix <steniafelix@gmail.com>
2019-10-10 02:34:23 -0300

    Configuring request to profile activation

--- src/pages/Active/index.js
+++ src/pages/Active/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from "../../services/api";
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import logo from "../../assets/logoTemp.png";
+
+import { Container } from './styles';
 
 export default function Active() {
+  const id_hash = useParams().id;
+
+  useEffect(() => {
+    async function load() {
+      await api.put(`/sessions/${id_hash}`)
+    }
+
+    load();
+  }, );
   
   return (
-    <h1>Ativação</h1>,
-    <p>Conta Ativada</p>
-  );
+    <Container>
+      <img src={logo} alt="mytest"/>
+      <h1>Conta ativada!</h1>
+      
+      <p>Realize seu login</p>
+      <Link to="/entrar"><button type="submit">Login</button></Link>
+      
+    </Container>
+  )
 }
--- src/pages/Active/styles.js
+++ src/pages/Active/styles.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components';
+import { darken } from "polished";
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;  
+
+  p {
+    font-size: 16px;
+    color: #333;
+  }
   
+
+  button {
+    width: 100%;
+    margin: 5px 0 0;
+    height: 44px;
+    background: #40E0D0;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.09, "#40E0D0")}
+    }
+  }
 `;
+
