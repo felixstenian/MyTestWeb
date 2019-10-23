75e34797841c2c9f215d62e61260eb37355b7a38
Stenia Felix <steniafelix@gmail.com>
2019-10-23 01:04:03 -0300

    Corrections

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://851b5fd6.ngrok.io',
-  // baseURL: "http://localhost:3333"
-})
+  // baseURL: 'http://',
+  baseURL: "http://localhost:3333"
+});
 
 export default api;
